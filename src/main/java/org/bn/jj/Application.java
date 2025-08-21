d3fbc385a3d1d67b332015a62871bcb361d1408a
Elena Vasilyeva <elena.vasilyeva@piano.io>
2025-08-22 01:09:31 +0400

    Initial commit

---
+++ src/main/java/org/bn/jj/Application.java
@@ -0,0 +1,13 @@
+package org.bn.jackandjill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JackAndJillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JackAndJillApplication.class, args);
+    }
+
+}
