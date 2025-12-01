8e96f76b32fbacf5530267312716efbc925a0529
belkanaphan <belkanafan@gmail.com>
2025-12-01 14:15:14 +0400

    ActivityUserDto

--- src/main/java/org/bn/sensation/core/activityuser/service/mapper/ActivityUserDtoMapper.java
+++ src/main/java/org/bn/sensation/core/activityuser/service/mapper/ActivityUserDtoMapper.java
@@ -1,12 +1,13 @@
 package org.bn.sensation.core.activityuser.service.mapper;
 
 import org.bn.sensation.core.activityuser.entity.ActivityUserEntity;
+import org.bn.sensation.core.activityuser.service.dto.ActivityUserDto;
 import org.bn.sensation.core.common.mapper.BaseDtoMapper;
 import org.bn.sensation.core.common.mapper.EntityLinkMapper;
-import org.bn.sensation.core.activityuser.service.dto.ActivityUserDto;
+import org.bn.sensation.core.user.service.mapper.UserDtoMapper;
 import org.mapstruct.Mapper;
 
-@Mapper(config = BaseDtoMapper.class, uses = {EntityLinkMapper.class})
+@Mapper(config = BaseDtoMapper.class, uses = {EntityLinkMapper.class, UserDtoMapper.class})
 public interface ActivityUserDtoMapper extends BaseDtoMapper<ActivityUserEntity, ActivityUserDto> {
 
     @Override
